3c196c80879e8e4becbf62086622a2d514f0b224
TheodorFurtuna <80576577+TheodorFurtuna@users.noreply.github.com>
2024-06-17 17:56:32 +0300

    Modifications8

--- app/src/main/java/com/example/android_project/fragments/MoviesFragment.kt
+++ app/src/main/java/com/example/android_project/fragments/MoviesFragment.kt
@@ -7,12 +7,28 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.android_project.R
 import com.example.android_project.adapters.WatchlistListAdapter
+import com.example.android_project.models.CartItemModel
 import com.example.android_project.models.CategoryModel
 import com.example.android_project.models.WatchlistModel
+import com.example.android_project.models.api.WatchlistAPIResponse
+import com.example.android_project.utils.extenstions.handleWatchlistResponse
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MoviesFragment: Fragment() {
+
+    private val watchlistItemList by lazy {
+        ArrayList<CartItemModel>()
+    }
+    private val adapter by lazy {
+        WatchlistListAdapter(watchlistItemList)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,82 +39,64 @@ class MoviesFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView()
+//        getWatchlist()
     }
 
     private fun setupRecyclerView(){
         val layoutManager = LinearLayoutManager(context)
 
-        val movieList = listOf(
-            CategoryModel(
-                name = "Category name 1",
-                description = "Category description 1"
-            ),
-            WatchlistModel(
-                name = "Movie name 1",
-                description = "Movie description 1"
-            ),
-            WatchlistModel(
-                name = "Movie name 2",
-                description = "Movie description 2"
-            ),
-            WatchlistModel(
-                name = "Movie name 3",
-                description = "Movie description 3"
-            ),
-            WatchlistModel(
-                name = "Movie name 4",
-                description = "Movie description 4"
-            ),
-            WatchlistModel(
-                name = "Movie name 5",
-                description = "Movie description 5"
-            ),
-            WatchlistModel(
-                name = "Movie name 5",
-                description = "Movie description 5"
-            ),
-            WatchlistModel(
-                name = "Movie name 5",
-                description = "Movie description 5"
-            ),
-            WatchlistModel(
-                name = "Movie name 5",
-                description = "Movie description 5"
-            ),
-            WatchlistModel(
-                name = "Movie name 5",
-                description = "Movie description 5"
-            ),            WatchlistModel(
-                name = "Movie name 5",
-                description = "Movie description 5"
-            ),
-            WatchlistModel(
-                name = "Movie name 5",
-                description = "Movie description 5"
-            ),
-            WatchlistModel(
-                name = "Movie name 5",
-                description = "Movie description 5"
-            ),
-            WatchlistModel(
-                name = "Movie name 5",
-                description = "Movie description 5"
-            ),
-            WatchlistModel(
-                name = "Movie name 5",
-                description = "Movie description 5"
-            ),
-            WatchlistModel(
-                name = "Movie name 5",
-                description = "Movie description 5"
-            )
-        )
-
-        val adapter = WatchlistListAdapter(movieList)
 
         view?.findViewById<RecyclerView>(R.id.rv_watchlist)?.apply{
             this.layoutManager = layoutManager
-            this.adapter = adapter
+            this.adapter = this@MoviesFragment.adapter
         }
+        fun getWatchlist(){
+
+            val queue = Volley.newRequestQueue(context)
+            val url = "https://www.fakestoreapi.com/products"
+
+            val stringRequest = StringRequest(
+                Request.Method.GET, url,
+                { response ->
+                    handleWatchlistResponse(response)
+                },
+                {
+                    "That didn't work!"
+                })
+
+            queue.add(stringRequest)
+
+        }
+        fun handleWatchlistResponse(response: String) {
+            val type = object: TypeToken<List<WatchlistAPIResponse>>(){}.type
+            val responseJsonArray = Gson().fromJson<List<WatchlistAPIResponse>>(response, type)
+
+            responseJsonArray
+                .groupBy{it.category}
+                .forEach{//it:Map.Entry<string, List<watchlistAPIResponse>>
+                    val categoryModel = CategoryModel(
+                        name = it.key,
+                        description =  it.key
+                    )
+                    val watchlist = it.value.map{ watchlistApi ->
+                        WatchlistModel(
+                            name = watchlistApi.name,
+                            description = watchlistApi.description
+                        )
+                    }
+                    watchlistItemList.add(categoryModel)
+                    watchlistItemList.addAll(watchlist)
+                }
+            adapter.notifyItemRangeInserted(0, watchlistItemList.size)
+//          adapter.notifyDataSetChanged()
+        }
+
+
+//        val adapter = WatchlistListAdapter(movieList)
+//
+//        view?.findViewById<RecyclerView>(R.id.rv_watchlist)?.apply{
+//            this.layoutManager = layoutManager
+//            this.adapter = adapter
+//        }
     }
 }
